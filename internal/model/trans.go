68a81c53f933524017c48c26474bce9c474c3eb9
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-06 08:31:54 -0800

    Add generation support to model.Trans

--- internal/model/trans.go
+++ internal/model/trans.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"encoding/json"
+	"math"
+	"math/rand"
 
 	"github.com/jcorbin/markov/internal/symbol"
 )
@@ -31,6 +33,40 @@ func (ts Trans) AddChain(chain []symbol.Symbol) {
 		last = sym
 	}
 	ts.Add(last, symbol.Symbol(0), 1)
+}
+
+// GenChain generates a chain through the transition table, using the supplied,
+// and calling the given function with each generated symbol. If the function
+// returns an error, generation stop annd it is returned.
+func (ts Trans) GenChain(rng *rand.Rand, f func(symbol.Symbol) error) error {
+	return ts.GenReducedChain(rng, func(sym symbol.Symbol) (symbol.Symbol, error) {
+		return sym, f(sym)
+	})
+}
+
+// GenReducedChain generates a reduced chain through the transition table. The
+// only difference from GenChain is that the function may influence the next
+// symbol; i.e. by using some sort of reduction logic to combine symbols under
+// higher-order language semantics (e.g. n-grams).
+func (ts Trans) GenReducedChain(rng *rand.Rand, f func(symbol.Symbol) (symbol.Symbol, error)) error {
+	var last symbol.Symbol
+	for {
+		var next symbol.Symbol
+		best := 1.0
+		for sym, w := range ts[last] {
+			if score := math.Pow(rng.Float64(), 1/float64(w)); score < best {
+				best, next = score, sym
+			}
+		}
+		next, err := f(next)
+		if err != nil {
+			return err
+		}
+		if next == symbol.Symbol(0) {
+			return nil
+		}
+		last = next
+	}
 }
 
 type jsonWS struct {
@@ -77,5 +113,3 @@ func (ts *Trans) UnmarshalJSON(data []byte) error {
 	}
 	return nil
 }
-
-// TODO: generating from a table
