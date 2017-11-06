7336912ce296beed614bdb901f537c7694cf666c
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-06 07:54:08 -0800

    Add model.DocInfo.Load

--- internal/model/db.go
+++ internal/model/db.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"os"
+)
+
 // DocDB represents a database of extracted documents. It contains a markov
 // language for generated a plausible document title, and an inverted index to
 // map back to supporting document info from such a title.
@@ -22,4 +27,25 @@ type Doc struct {
 	Title string            `json:"title"`
 	Info  map[string]string `json:"info"`
 	Lang  Lang              `json:"language"`
+}
+
+// Load loads the extracted document from TransFile; subsequent calls to Load
+// may return the same *Doc pointer.
+func (di DocInfo) Load() (rd *Doc, rerr error) {
+	// TODO: caching
+	f, err := os.Open(di.TransFile)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if cerr := f.Close(); rerr == nil {
+			rerr = cerr
+		}
+	}()
+	var d Doc
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&d); err != nil {
+		return nil, err
+	}
+	return &d, nil
 }
